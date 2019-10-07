fb6f74e5068f6d055eec455f8c36e062bb841ca0
Joel Pina <joelpina93@gmail.com>
2019-10-07 13:31:44 -0300

    Creation of SystemParking class and SystemParkingTest UnitTest

---
+++ Parking/BusinessLogic/ParkingSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class ParkingSystem
+    {
+        public List<Account> AccountList { get; set; }
+
+        public ParkingSystem()
+        {
+            AccountList = new List<Account>();
+        }
+
+        public bool NoAccountInSystem()
+        {
+            return AccountList.Count == 0;
+        }
+
+        public bool CorrectAccountCellPhone()
+        {
+            return true;
+        }
+
+        public void AddAcount(Account userAccount)
+        {
+            AccountList.Add(userAccount);
+        }
+    }
+}
---
+++ Parking/BusinessLogicTest/SystemParkingTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class SystemParkingTest
+    {
+        [TestMethod]
+        public void SystemWithNoAccountTest()
+        {
+            ParkingSystem system = new ParkingSystem();
+            Assert.IsTrue(system.NoAccountInSystem());
+        }
+
+        [TestMethod]
+        public void SystemWithAccountsTest()
+        {
+            ParkingSystem system = new ParkingSystem();
+            Account userAccount = new Account();
+            system.AddAcount(userAccount);
+            Assert.IsFalse(system.NoAccountInSystem());
+        }
+
+        [TestMethod]
+        public void SystemAddCorrectAccountTest()
+        {
+            ParkingSystem system = new ParkingSystem();
+            Assert.IsTrue(system.NoAccountInSystem());
+        }
+    }
+}
