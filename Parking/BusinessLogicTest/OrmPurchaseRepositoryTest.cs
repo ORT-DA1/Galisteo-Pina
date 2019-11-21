7e26f7241d6f8a0c76b94a8fbf989f825742bb6d
Joel <Joelpina93@gmail.com>
2019-11-21 18:20:22 -0300

    Bug Fixes

--- Parking/BusinessLogicTest/OrmPurchaseRepositoryTest.cs
+++ Parking/BusinessLogicTest/OrmPurchaseRepositoryTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using Data;
 using Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -48,6 +50,52 @@ namespace BusinessLogicTest
             ParkingContext.SaveChanges();
             Assert.IsTrue(ormPurchaseRepository.AnyPurchaseMatchesPlateAndDateTime("ABC1234", hourForTest.ToString("HH:mm")));
         }
+
+        [TestMethod]
+        public void GetPurchasesMatchDateAndCountryTest()
+        {
+            ormPurchaseRepository.AddPurchase(TestPurchase, 1);
+            ParkingContext.SaveChanges();
+            IEnumerable<Purchase> matchingPurchases = ormPurchaseRepository.GetPurchasesMatchDateAndCountry(TestPurchase.Sms.StartingHour, TestPurchase.Sms.EndingHour, "ABC1234");
+            Assert.AreEqual(matchingPurchases.ToList().Count, 1);
+        }
+
+        [TestMethod]
+        public void GetPurchasesMatchDateAndCountryWithCountryParamTest()
+        {
+            Country country = new Country("PARAGUAY");
+            TestPurchase.Account.AccountCountry = country;
+            ormPurchaseRepository.AddPurchase(TestPurchase, 1);
+            ParkingContext.SaveChanges();
+            IEnumerable<Purchase> matchingPurchases = ormPurchaseRepository.GetPurchasesMatchDateAndCountry(TestPurchase.Sms.StartingHour, TestPurchase.Sms.EndingHour, "ABC1234", country);
+            Assert.AreEqual(matchingPurchases.ToList().Count, 1);
+        }
+
+        [TestMethod]
+        public void GetPurchasesMatchDateAndCountryWithWrongDatesFalseTest()
+        {
+            Country country = new Country("PARAGUAY");
+            TestPurchase.Account.AccountCountry = country;
+            ormPurchaseRepository.AddPurchase(TestPurchase, 1);
+            ParkingContext.SaveChanges();
+            IEnumerable<Purchase> matchingPurchases = ormPurchaseRepository
+                .GetPurchasesMatchDateAndCountry(TestPurchase.Sms.StartingHour.AddHours(2),
+                TestPurchase.Sms.EndingHour.AddHours(2), "ABC1234", country);
+            Assert.AreNotEqual(matchingPurchases.ToList().Count, 1);
+        }
+
+        [TestMethod]
+        public void GetPurchasesMatchDateAndCountryWithCountryParamFalseTest()
+        {
+            Country country = new Country("NICARAGUA");
+            Country country2 = new Country("CAMBODIA");
+            TestPurchase.Account.AccountCountry = country;
+            ormPurchaseRepository.AddPurchase(TestPurchase, 1);
+            ParkingContext.SaveChanges();
+            IEnumerable<Purchase> matchingPurchases = ormPurchaseRepository.GetPurchasesMatchDateAndCountry(TestPurchase.Sms.StartingHour, TestPurchase.Sms.EndingHour, "ABC1234", country2);
+            Assert.AreNotEqual(matchingPurchases.ToList().Count, 1);
+        }
+
 
         [TestCleanup]
         public void CleanUp()
